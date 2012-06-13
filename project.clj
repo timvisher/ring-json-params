be3b4bd57dbc5520eb720ce3c78be8879afc0cae
Tim Visher <tim.visher@siemens.com>
2012-06-13 12:02:36 -0400

    Using my clojars group. EOM

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject timvisher/ring-json-params "0.1.4-SNAPSHOT"
+(defproject org.clojars.timvisher/ring-json-params "0.1.4-SNAPSHOT"
   :description "Ring middleware for JSON params parsing."
   :dependencies
    [[org.clojure/clojure "1.3.0"]
